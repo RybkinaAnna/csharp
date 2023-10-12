a3164dcdbbed1bc30ae73a028607a9c4d628d535
Anya Rybkina <anuta.ribkina@gmail.com>
2023-10-12 11:58:15 +0200

    дз

---
+++ task015_выходной/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int dayNumber = ReadInt("Введите число от 1 до 7: ");
+Console.WriteLine(WorkHoliday(dayNumber));
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+string WorkHoliday(int a)
+{
+    if (a > 0 && a < 8)
+    {
+        if (a == 7 || a == 6)
+        {
+            Console.Write("да");
+        }
+        else
+        {
+            Console.Write("нет");
+        }
+    }
+    else
+    {
+        Console.Write("Вы ввели число не от 1 до 7");
+    }
+    return" ";
+}
