f6b672d9a02cdd5397a41ad8a481770b918509d1
Anya Rybkina <anuta.ribkina@gmail.com>
2023-10-19 13:27:08 +0200

    урок_4

---
+++ task032_массив/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+32. Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+*/
+
+Console.Clear();
+
+int[] GetArray(int start, int end, int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i]);
+        if (i < arr.Length - 1) System.Console.Write(", ");
+    }
+    System.Console.Write("]");
+    Console.ResetColor();
+}
+
+int[] ChangeArray(int[] arrMax)
+{
+    System.Console.WriteLine();
+    for (int i = 0; i < arrMax.Length ; i++)
+    {
+        arrMax[i] *= -1;
+    }
+    return arrMax;
+}
+
+
+
+System.Console.WriteLine("Задай диапазон массива от: ");
+int userStart = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Задай диапазон массива до: ");
+int userEnd = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Задай размер массива: ");
+int userSize = Convert.ToInt32(Console.ReadLine());
+
+
+int[] userArray = GetArray(userStart, userEnd, userSize);
+
+PrintArray(userArray);
+
+int[] userArraySec = ChangeArray (userArray);
+PrintArray(userArraySec);
+
+
---
+++ task033_рандом/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+33. Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+-3; массив [6, 7, 19, 345, 3] -> да
+*/
+
+int[] RandomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine($"{arr[i]}");
+    }
+}
+
+
+bool FindNumber(int[] arrSec, int numb )
+{
+    bool result = false;
+    for (int i = 0; i < arrSec.Length; i++)
+    {
+        if (arrSec[i] == numb)
+        result = true;
+    }
+    return result;
+}
+
+int[] userArray = RandomArray();
+PrintArray(userArray);
+System.Console.WriteLine("Введите число: ");
+int numb  = convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(FindNumber(userArray, numb));
+
